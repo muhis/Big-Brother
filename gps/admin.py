96055e90130a0342b792faf576dc383315ba717d
Mohammed Salman <m.salman@zinkki.com>
2018-01-17 21:05:20 +0200

    Implement model coordinate

---
+++ gps/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from gps.models import Coordinate
+# Register your models here.
+
+admin.site.register(Coordinate)
--- gps/models.py
+++ gps/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User as auth_User
 # Create your models here.
+
+
+class Coordinate(models.Model):
+    """
+    One coordinate reported
+    """
+    longitude = models.CharField(max_length=10)
+    latitude = models.CharField(max_length=10)
+    user = models.ForeignKey(auth_User, on_delete=models.CASCADE)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
